54a6b3830e9d71fdd5cc15892d2749dff6395c22
undermountain <y@179.jp>
2022-12-23 18:12:14 +0900

    [Add] 間取り図周りの実装

--- src/components/VrMap.jsx
+++ src/components/VrMap.jsx
@@ -1,9 +1,7 @@
 import { jsx, css, keyframes } from '@emotion/react'
 import Config from '../Config'
 
-import mapImage from '../assets/map.jpg'
-
-import '../css/VrMap.css'
+import plan from '../assets/Plan.png'
 
 function VrMap(props) {
   const mapClass = props.showMap ? 'isShow' : ''
@@ -19,65 +17,54 @@ function VrMap(props) {
     return (
       <li
         key={v}
-        css={css}
+        className="pointLink"
         style={position}
-        onClick={() => props.nodeChangeHandler(v)}></li>
+        onClick={() => props.viewPointChangeHandler(v)}></li>
     )
   })
   return (
     <nav className={mapClass} css={mapStyle}>
-      <p css={mapTitle}>Map</p>
-      <p css={closeBtn} onClick={() => props.toggleMapVisible()}></p>
-      <ul css={styles.pointLinkWrap}>
-        {points}
+      <ul css={mapTitle}>
+        <li className="type">ルームプラン</li>
+        <li className="name isAlphabet">Type A</li>
+        <li className="plan isAlphabet">4LDK + SC</li>
+        <li className="floor_space">住居専有面積<span className="isAlphabet">82.04</span>㎡</li>
       </ul>
-      <img className="Map_img" src={mapImage} />
+      <p css={closeBtn} onClick={() => props.toggleMapVisible()}></p>
+      <div className="mapWrap">
+        <ul className="pointLinkWrap">
+          {points}
+        </ul>
+        <img className="mapImage" src={plan} />
+      </div>
     </nav>
   )
 }
 
 const ripple1 = keyframes`
   0% {
-    transform: scale(1);
+    transform: translate(-9px, -9px) scale(1);
   }
   50% {
-    transform: scale(0.85);
+    transform: translate(-9px, -9px) scale(0.85);
   }
   100% {
-    transform: scale(1);
+    transform: translate(-9px, -9px) scale(1);
   }
 `;
 const ripple2 = keyframes`
   0% {
-    transform: scale(1);
+    transform: translate(-9px, -9px) scale(1);
   }
   50% {
-    transform: scale(1.2);
+    transform: translate(-9px, -9px) scale(1.2);
   }
   100% {
-    transform: scale(1);
+    transform: translate(-9px, -9px) scale(1);
   }
 `;
 
 const styles = {
-  pointLinkWrap: {
-    height: '100%',
-    margin: 0,
-    padding: 0,
-    position: "absolute",
-    width: '100%',
-    zIndex: 500,
-  },
-  pointLink: {
-    animation: `${ripple1} 1.5s ease infinite`,
-    border: `4px solid ${Config.color.main}`,
-    borderRadius: '50%',
-    boxShadow: '-2px 0 6px 2px rgba(0,0,0,.4)',
-    height: '24px',
-    listStyle: 'none',
-    position: "absolute",
-    width: '24px',
-  },
   isCurrent: {
     animation: `${ripple2} 1.5s ease infinite`,
     backgroundColor: `${Config.color.main}`,
@@ -85,7 +72,9 @@ const styles = {
 }
 
 const mapStyle = css`
-  border-left: 1px solid #B19C83;
+  background-color: rgba(45, 45, 45, .5);
+  backdrop-filter: blur(4px);
+  // border-left: 1px solid ${Config.color.main};
   box-shadow: -8px 0 10px rgba(0, 0, 0, .3);
   height: 100%;
   position: absolute;
@@ -94,27 +83,86 @@ const mapStyle = css`
   pointer-events: auto;
   right: -150%;
   transition: all 850ms;
+  
+  width: 70%;
+  max-width: 480px;
+
   &.isShow {
-    border-left: 6px solid #B19C83;
+    // border-left: 6px solid ${Config.color.main};
     right: 0;
     transition: all 650ms;
   }
+  .mapWrap {
+    position: relative;
+    transform: translateY(50%);
+    width: 100%;
+  }
+  .pointLinkWrap {
+    height: 100%;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 500;
+  }
+  .pointLink {
+    animation: ${ripple1} 1.5s ease infinite;
+    border: 4px solid ${Config.color.main};
+    border-radius: 50%;
+    box-shadow: -2px 0 6px 2px rgba(0,0,0,.4);
+    cursor: pointer;
+    height: 18px;
+    list-style: none;
+    position: absolute;
+    width: 18px;
+  }
+  .mapImage {
+    height: auto;
+    width: 100%;
+  }
 `
 const mapTitle = css`
-  color: #272626;
-  font-size: 58px;
-  font-family: ${Config.font.family};
-  font-weight: ${Config.font.weight};
-  font-style: ${Config.font.style};
-  left: ${Config.grid * 5}px;
+  color: #FFF;
+  left: ${Config.grid * 8}px;
+  line-height: 1.4;
+  list-style: none;
   margin: 0;
+  padding: 0;
   position: absolute;
   text-shadow: 0 0 3px rgba(0,0,0,.2);
   top: ${Config.grid * 2}px;
   transition: left 650ms;
   transition-delay: 300ms;
   .isShow & {
-    left: ${Config.grid * (-1.5)}px;
+    left: ${Config.grid * 3}px;
+  }
+  .isAlphabet {
+    font-family: ${Config.font.family};
+    font-weight: ${Config.font.weight};
+    font-style: ${Config.font.style};
+  }
+  .type {
+    font-size: 16px;
+    line-height: 1;
+    margin-bottom: ${Config.grid * 3.5}px;
+  }
+  .name {
+    font-size: 54px;
+    line-height: 1;
+    margin-bottom: ${Config.grid}px;
+  }
+  .plan {
+    font-size: 24px;
+    margin-bottom: ${Config.grid * 1.5}px;
+  }
+  .floor_space {
+    font-size: 14px;
+    .isAlphabet {
+      font-size: 24px;
+      padding: 0 ${Config.grid * 1}px;
+    }
   }
 `
 
@@ -126,13 +174,13 @@ const closeBtn = () => [
     margin: 0;
     position: absolute;
     pointer-events: auto;
-    right: ${Config.grid * 1.5}px;
-    top: ${Config.grid * 3}px;
+    right: ${Config.grid * 2}px;
+    top: ${Config.grid * 2}px;
     width: ${Config.grid * 3}px;
     z-index: 550;
     &:before,
     &:after {
-      background-color: #272626;
+      background-color: #FFF;
       content: "";
       display: inline-block;
       height: ${Config.grid * 4}px;
